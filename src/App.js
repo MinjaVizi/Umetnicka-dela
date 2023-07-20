83802b1d815b66ebb943eec01187d7293798874d
MinjaVizi <viziminja1509@gmail.com>
2023-07-20 12:52:45 +0200

    dodat footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import Navbar from './komponente/Navbar';
 import Pocetna from './komponente/pocetna'
 import Kontakt from './komponente/kontakt';
+import Proizvodi from './komponente/Proizvodi';
+import Footer from './komponente/Footer';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 
 function App() {
@@ -13,7 +15,8 @@ function App() {
 
         <BrowserRouter>
         <Navbar></Navbar>
-        
+        <Proizvodi></Proizvodi>
+
         <Routes>
           <Route path='/' element={<Pocetna></Pocetna>}> </Route>
           <Route path='/kontakt' element= {<Kontakt></Kontakt>}> </Route>
@@ -25,7 +28,7 @@ function App() {
         </Routes>
 
 
-
+<Footer></Footer>
   
         </BrowserRouter>
     </div>
